73761265b4fda7fd09b49c65855ea75870c1a4a3
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-12 09:50:07 +0000

    Completed variables and basic datatypes

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Variables;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        int x;
+        int y;
+        x = 3;
+        y = x + 9;
+        Console.WriteLine(x + y);
+        Console.ReadLine();
+        */
+
+        Console.WriteLine("What is your name?");
+
+        string myFirstName;
+        Console.Write("Your first name: ");
+        myFirstName = Console.ReadLine();
+
+        string myLastName;
+        Console.Write("Your last name: ");
+        myLastName = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+        Console.ReadLine();
+    }
+}
+
