23ec4ee2134d873486096bf846761ac7d50dfe6e
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-13 14:27:53 +0000

    Completed for iteration topic

---
+++ ForIteration/ForIteration/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForIteration;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i=1; i<=10; i++)
+            {
+            if (i%2 == 0)
+            {
+                continue;
+                //Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+            }
+        Console.ReadLine();
+    }
+}
+
