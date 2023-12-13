e28e84a8d59ff3f5d2ef5c0bd494416a34c30776
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-13 15:42:45 +0000

    Completed the basics of arrays

---
+++ UnderstandingArrays/UnderstandingArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace UnderstandingArrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // int[] numbers = new int[5];
+        int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+        string[] names = new string[] { "Mohan", "Sindhu", "Jaisrith" };
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        Array.Reverse(names);
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+        char[] chararray = names[1].ToCharArray();
+        Array.Reverse(chararray);
+        foreach (char charname in chararray)
+        {
+            Console.Write(charname);
+        }
+        Console.ReadLine();
+        
+    }
+
+   
+}
+
