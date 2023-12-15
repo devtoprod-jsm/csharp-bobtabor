d1d37bee03c39a1f912e244dd7170ef25d595469
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-15 10:55:16 +0000

    Completed basics on scopes and accessibility modifiers

---
+++ UnderstandingScope/UnderstandingScope/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace UnderstandingScope;
+class Program
+{
+    private static string k = "";
+    static void Main(string[] args)
+    {
+        string j = "";
+        for (int i=1; i<11; i++)
+        {
+            Console.WriteLine(i);
+            j = i.ToString();
+            k = i.ToString();
+
+        }
+        Console.WriteLine("j outside the loop {0}", j);
+        HelperMethod();
+        Console.ReadLine();
+        
+    }
+
+    private static void HelperMethod()
+    {
+        Console.WriteLine("k in helpermethod {0}", k);
+    }
+}
+
