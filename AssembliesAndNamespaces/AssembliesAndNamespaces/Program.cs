82416306d4b8de8a6e678c067a1083320b944e76
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-16 00:19:12 +0000

    Completed basics of namespaces and assemblies, their purpose and usage

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace AssembliesAndNamespaces;
+class Program
+{
+    static void Main(string[] args)
+    {
+        WebClient client = new WebClient();
+        string reply = client.DownloadString("https://msdn.microsoft.com");
+        //File.WriteAllText("/tmp",reply);
+        Console.WriteLine(reply);
+        Console.ReadLine();
+
+    }
+}
+
