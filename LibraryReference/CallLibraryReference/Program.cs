564cfcae2f63b645fc96f8a09139544e8c6ad849
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-18 16:33:08 +0000

    Completed basics on assembly and library reference

---
+++ LibraryReference/CallLibraryReference/Program.cs
@@ -0,0 +1,14 @@
+﻿using LibraryReference;
+
+namespace CallLibraryReference;
+class Program
+{
+    static void Main(string[] args)
+    {
+        LibRef refLib = new LibRef();
+        string reply = refLib.scrapeWebpage("http://msdn.microsoft.com");
+        Console.WriteLine(reply);
+        Console.ReadLine();
+    }
+}
+
---
+++ LibraryReference/LibraryReference/LibRef.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace LibraryReference;
+public class LibRef
+{
+    public string scrapeWebpage(string url)
+    {
+        return getWebpage(url);
+    }
+
+    public string scrapeWebpage(string filepath,string url)
+    {
+        string reply = getWebpage(url);
+        File.WriteAllText(filepath, reply);
+        return reply;
+    }
+
+    private string getWebpage(string url)
+    {
+        WebClient client = new WebClient();
+        return client.DownloadString(url);
+    }
+}
+
