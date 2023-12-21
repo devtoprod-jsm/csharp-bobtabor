fe288da9ece430ec04256dd555d673ba2996ae74
devtoprod-jsm <53889173+devtoprod-jsm@users.noreply.github.com>
2023-12-21 23:58:44 +0000

    Completed basic event handling scenario

---
+++ EventDrivenTimer/EventDrivenTimer/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EventDrivenTimer;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Timers.Timer myTimer = new System.Timers.Timer(2000);
+        myTimer.Elapsed += MyTimer_Elapsed;
+        myTimer.Elapsed += MyTimer_Elapsed1;
+        Console.WriteLine("Press enter to remove blue display");
+        myTimer.Start();
+        Console.ReadLine();
+        myTimer.Elapsed -= MyTimer_Elapsed1;
+        Console.ReadLine();
+    }
+
+    private static void MyTimer_Elapsed1(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Elapsed1: {0:hh:mm:ss.fff}", e.SignalTime);
+    }
+
+    private static void MyTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Elapsed: {0:hh:mm:ss.fff}",e.SignalTime);
+    }
+}
---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HandlingExceptions;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            String content = File.ReadAllText(@"/Users/mohantella/Documents/github-recovery-codes.tx");
+            Console.WriteLine(content);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine("Path is wrong. Please verify the path");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            //Code to Finalize
+            //Setting objects to null
+            //Closing database connections
+            Console.WriteLine("Finally...!!!");
+        }
+        Console.ReadLine();
+    }
+   
+}
+
